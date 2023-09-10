f90d64a013013f672d0a66f6c40c896fda05f830
chaitanyadodda <101582834+chaitanyadodda@users.noreply.github.com>
2023-09-10 17:34:58 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,8 +14,8 @@ provider "aws" {
   access_key = ""
   secret_key = ""
 =======
-  access_key = "AKIA6K7IQLMRO7YU6DGW"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 >>>>>>> a2d2eac (added files)
 }
 
